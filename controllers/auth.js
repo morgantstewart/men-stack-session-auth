514abfae32fb3b695a361b27631987be83340406
morgantstewart <morgan@paysigil.com>
2025-07-22 13:03:07 -0400

    fixed signup issues

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,13 +1,45 @@
+const User = require("../models/user.js");
+const bcrypt = require("bcrypt");
+
 const express = require("express");
 const router = express.Router();
 
 
 
 router.get("/sign-up", (req, res) => {
-  res.render("auth/sign-up.ejs");
+    res.render("auth/sign-up.ejs");
 });
 
 
+
+router.post("/sign-up", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    if (userInDatabase) {
+        return res.send("Username already taken.");
+    }
+
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.send("Password and Confirm Password must match");
+    }
+
+
+    const hashedPassword = bcrypt.hashSync(req.body.password, 10);
+
+    req.body.password = hashedPassword;
+
+    const user = await User.create(req.body);
+
+    res.send(`Thanks for signing up ${user.username}`);
+
+});
+
+
+
+router.get("/sign-in", (req, res) => {
+res.render("auth/sign-in.ejs");
+
+});
 
 
 module.exports = router;
