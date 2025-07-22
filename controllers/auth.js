f91520878e2004986a1bd4b16bb1569a6dae5812
morgantstewart <morgan@paysigil.com>
2025-07-22 13:11:59 -0400

    added password wrong functionality

--- controllers/auth.js
+++ controllers/auth.js
@@ -36,9 +36,28 @@ router.post("/sign-up", async (req, res) => {
 
 
 
-router.get("/sign-in", (req, res) => {
-res.render("auth/sign-in.ejs");
+router.get("/sign-in", async (req, res) => {
+    res.render("auth/sign-in.ejs");
 
+});
+
+router.post("/sign-in", async (req, res) => {
+    const userInDatabase = await User.findOne({ username: req.body.username });
+    
+    if (!userInDatabase) {
+        return res.send("Login failed. Please try again.");
+    }
+
+
+    const validPassword = bcrypt.compareSync(
+        req.body.password,
+        userInDatabase.password
+    );
+    if (!validPassword) {
+        return res.send("Login failed. Please try again.");
+    }
+
+    res.send("Request to sign in received!");
 });
 
 
